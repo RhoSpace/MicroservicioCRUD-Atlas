262317caae976a382d8fa84c558fa1ba48447d1f
Rodrigo Rivera <rodrigo.rivera.b@hotmail.com>
2023-03-30 18:50:09 -0300

    Vercel.json server

--- src/routes/product.js
+++ src/routes/product.js
@@ -29,7 +29,7 @@ router.get("/products/:id", (req, res) => {
 router.delete("/products/:id", (req, res) => {
   const { id } = req.params;
   productSchema
-    .remove({ _id: id })
+    .deleteOne({ _id: id })
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
